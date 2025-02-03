eb2ee619c45bdfd4db8839f6f70c06470f91c26a
10sandeep <sandeepnayak1424@gmail.com>
2025-02-03 23:48:00 +0530

    added login logout functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,33 @@
-import {LOGO_URL} from '../utils/constants';
+import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
 
 const Header = () => {
-
-  const [btnnameReact,setBtnNameReact] = useState("Login")
-    return (
-      <div className="header">
-        <div className="logo-container">
-          <img
-            className="logo"
-            src={LOGO_URL}
-            alt="Logo"
-          />
-        </div>
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-            <button className='login' onClick={()=>{
-              setBtnNameReact("Logout")
-            }}>{btnnameReact}</button>
-          </ul>
-        </div>
+  const [btnnameReact, setBtnNameReact] = useState("Login");
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={LOGO_URL} alt="Logo" />
       </div>
-    );
-  };
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnnameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnnameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-  export default Header;
+export default Header;
